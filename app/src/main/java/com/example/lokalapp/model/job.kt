2afcdb0ab46a4508daf0e4e712adb30fcf83f751
Srinath-Gajula <srinathgajula321@gmail.com>
2025-04-10 22:49:20 +0530

    remove ?

--- app/src/main/java/com/example/lokalapp/model/job.kt
+++ app/src/main/java/com/example/lokalapp/model/job.kt
@@ -7,7 +7,7 @@ import androidx.room.PrimaryKey
 data class Job(
     @PrimaryKey val id: Int,
     val title: String,
-    val location: String?,
+    val location: String,
     val salary: String?,
     val phone: String?,
     val description: String?
